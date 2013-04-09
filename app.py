a323da8de254cdebae49925538edb316f60d8962
Eric Meadow <palmercurling@android.net>
2013-04-09 15:44:08 -0400

    added import

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import os
 import psycopg2
 import time
+from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
